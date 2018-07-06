df723384dbb6b4c2f02763958b5bf71aed5435cb
Levi Fuller <lfuller941@gmail.com>
2018-07-05 20:56:16 -0600

    Added support for URL decoding JSON strings; Added readme;

--- Program.cs
+++ Program.cs
@@ -11,39 +11,43 @@ namespace DashSerializer
       
         static void Main(string[] args)
         {
-            string encodedDashDescription = UrlEncodeDashDescription(TestData.DashDescriptionJson);
+            // URL encode all JSON string properties
+            string encodedDashDescription = UrlEncodeJsonStringProperties(TestData.DashDescriptionJson);
             // Log dash description with URL encoded strings
             Console.WriteLine(encodedDashDescription);
+            // URL decode all JSON string properties
+            string decodedDashDescription = UrlEncodeJsonStringProperties(encodedDashDescription, shouldDecode: true);
+            Console.WriteLine(decodedDashDescription);
             Console.ReadKey();
         }
 
-        private static string UrlEncodeDashDescription(string dashDescriptionJson) {
-            dynamic dashDescription = JsonConvert.DeserializeObject<dynamic>(dashDescriptionJson);
+        private static string UrlEncodeJsonStringProperties(string json, bool shouldDecode = false) {
+            dynamic dashDescription = JsonConvert.DeserializeObject<dynamic>(json);
             // Iterate over all root properties
             foreach (JProperty jProp in dashDescription) {
-                EncodeStringProperties(jProp);
+                EncodeStringProperties(jProp, shouldDecode);
             }
             return JsonConvert.SerializeObject(dashDescription);
         }
 
-        private static void EncodeStringProperties(JProperty jProp) {
+        private static void EncodeStringProperties(JProperty jProp, bool shouldDecode) {
             if (jProp.HasValues && jProp.Value.Type == JTokenType.String) {
                 // Check if property value is a string and encode it
-                jProp.Value = HttpUtility.UrlEncode(jProp.Value.ToString());
+                jProp.Value = shouldDecode ? HttpUtility.UrlDecode(jProp.Value.ToString()) : HttpUtility.UrlEncode(jProp.Value.ToString());
             } else if (jProp.HasValues) {
                 // Iterate all child properties
-                EncodeStringChildProperties(jProp);
+                EncodeStringChildProperties(jProp, shouldDecode);
             }
         }
 
-        private static void EncodeStringChildProperties(JToken jtoken) {
+        private static void EncodeStringChildProperties(JToken jtoken, bool shouldDecode) {
             foreach (JToken childJProp in jtoken.Children()) {
                 if (childJProp is JProperty) {
                     // If token is a JSON property, check if it's a string and encode it
-                    EncodeStringProperties(childJProp as JProperty);
+                    EncodeStringProperties(childJProp as JProperty, shouldDecode);
                 } else {
                     // Continue iterating through the children if it's not a property
-                    EncodeStringChildProperties(childJProp);
+                    EncodeStringChildProperties(childJProp, shouldDecode);
                 }
             }
         }
